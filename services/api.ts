03d0cc577d95715ebb4e1eebc340b4ff6841227f
Aurora <243707@ids.upchiapas.edu.mx>
2026-03-05 21:52:25 -0600

    correciones

--- services/api.ts
+++ services/api.ts
@@ -1,27 +1,25 @@
 import { Film, Person } from "@/types";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-
 export async function getAllFilms(): Promise<Film[]> {
-  const response = await fetch(`${BACKEND_URL}/api/films`);
+  const response = await fetch(`/api/films`);
   if (!response.ok) throw new Error("Error al obtener películas");
   return response.json();
 }
 
 export async function getFilmById(id: string): Promise<Film> {
-  const response = await fetch(`${BACKEND_URL}/api/films/${id}`);
+  const response = await fetch(`/api/films/${id}`);
   if (!response.ok) throw new Error("Película no encontrada");
   return response.json();
 }
 
 export async function getAllPeople(): Promise<Person[]> {
-  const response = await fetch(`${BACKEND_URL}/api/people`);
+  const response = await fetch(`/api/people`);
   if (!response.ok) throw new Error("Error al obtener personajes");
   return response.json();
 }
 
 export async function getPersonById(id: string): Promise<Person> {
-  const response = await fetch(`${BACKEND_URL}/api/people/${id}`);
+  const response = await fetch(`/api/people/${id}`);
   if (!response.ok) throw new Error("Personaje no encontrado");
   return response.json();
 }
